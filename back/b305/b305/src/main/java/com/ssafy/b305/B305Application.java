ae2ae533037d8414e5329fc00a1a9a35ee8471b6
육다빈 <yukdabin@naver.com>
2022-10-18 15:09:16 +0900

    feat : project init

---
+++ back/b305/b305/src/main/java/com/ssafy/b305/B305Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.b305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B305Application.class, args);
+	}
+
+}
