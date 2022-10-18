998a6434ad168e2d55829beaa495401f58e93bfd
seoeun98 <vcv0174@naver.com>
2022-10-18 16:13:07 +0900

    be : add schema creator

---
+++ back/b305/src/main/java/com/ssafy/b305/domain/entity/User.java
@@ -0,0 +1,30 @@
+package com.ssafy.b305.domain.entity;
+
+import lombok.*;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.annotations.DynamicInsert;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@DynamicInsert
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int u_id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String pw;
+
+    @Column
+    private String name;
+}
