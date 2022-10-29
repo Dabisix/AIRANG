8cd6dd9a2f9e872cbe939c112fb50e929df77b70
Sukong <sukong0507@gmail.com>
2022-10-29 22:34:14 +0900

    feat : #7 버튼 애니메이션 추가

--- front/airang/Assets/Scripts/StartScene/UserLogin.cs
+++ front/airang/Assets/Scripts/StartScene/UserLogin.cs
@@ -6,10 +6,22 @@ public class UserLogin : MonoBehaviour
 {
     public StartMainCamera my_camera;
     public DoorOpen doorOpen;
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     // 일단 임시로 마우스 클릭했을 때 카메라가 집안으로 이동하게 해보장.
     private void OnMouseDown()
     {
+        if (anim != null)
+        {
+            // play Bounce but start at a quarter of the way though
+            anim.Play("LoginButtonTouch", 0, 0.25f);
+        }
         my_camera.StartMoveCamera(1);
         Invoke("openDoor", 2.7f);
     }
