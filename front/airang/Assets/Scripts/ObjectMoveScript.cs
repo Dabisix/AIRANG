def95197c3b8e5e3101700d643bdc27dc436ec37
정수빈 <integerbin2@naver.com>
2022-11-02 11:09:12 +0900

    feat : #18 script를 나눔 (터치하는것과 타겟 pos 가진 것으로 나눔)

--- front/airang/Assets/Scripts/ObjectMoveScript.cs
+++ front/airang/Assets/Scripts/ObjectMoveScript.cs
@@ -6,11 +6,6 @@ public class ObjectMoveScript : MonoBehaviour
 {
     public AnimationCurve animationCurve;   //애니메이션 움직임 정도 (빠르게, 천천히)
     public Transform objectTransform;   //움직일 object
-    public bool isTrackable;    // 터치 따라다닐거냐
-    public bool useTargetPos;   //targetPos 넣을거냐
-    public bool useAnimation;   //animation 있는거냐
-    
-    public float[] delta = {-5, 0.5f, 0 }; //x, y, z
 
     private Vector3 startPos; //시작 위치
     public Vector3 targetPos; //목표 위치
@@ -23,22 +18,10 @@ public class ObjectMoveScript : MonoBehaviour
     void Start()
     {
        startPos = objectTransform.position;    //현재 위치
-        //터치 따라다닐 경우
-        if (isTrackable)
-        {
-            Debug.Log("준비중. GetComponent() 사용하여 ARSessionOrigin쪽에서 가져올 듯");
-        }
-        else if (!useTargetPos)
-        {
-            targetPos = startPos + new Vector3(delta[0], delta[1], delta[2]);
-        }
-        else
-        {
-            // startPos와 localPosition 비교하여 targetPos값 변경
-            targetPos.x = startPos.x - transform.localPosition.x + targetPos.x;
-            targetPos.y = startPos.y - transform.localPosition.y + targetPos.y;
-            targetPos.z = startPos.z - transform.localPosition.z + targetPos.z;
-        }
+        // startPos와 localPosition 비교하여 targetPos값 변경
+        targetPos.x = startPos.x - transform.localPosition.x + targetPos.x;
+        targetPos.y = startPos.y - transform.localPosition.y + targetPos.y;
+        targetPos.z = startPos.z - transform.localPosition.z + targetPos.z;
     }
 
 
@@ -47,23 +30,12 @@ public class ObjectMoveScript : MonoBehaviour
     {
         timer += Time.deltaTime;
         float percentage = timer / duration;
-        //터치 따라다닐 경우
-        if (isTrackable)
+        objectTransform.position = Vector3.Lerp(startPos, targetPos, animationCurve.Evaluate(percentage));
+        if (objectTransform.position == targetPos)  //위치 도착하면
         {
-            Debug.Log("준비중. GetComponent() 사용하여 ARSession~~");
-        }
-        else
-        {
-            objectTransform.position = Vector3.Lerp(startPos, targetPos, animationCurve.Evaluate(percentage));
-            if (objectTransform.position == targetPos)  //위치 도착하면
-            {
-                if (useAnimation)
-                {
-                    Animator animator = objectTransform.GetComponent<Animator>();
-                    animator.speed = 0.0f;  //애니메이션 멈추게
-                    animator.Play("Idle_A");    //기본 애니메이션으로 변경(점프하면서 멈추는 것 방지용)
-                }
-            }
+            Animator animator = objectTransform.GetComponent<Animator>();
+            animator.speed = 0.0f;  //애니메이션 멈추게
+            animator.Play("Idle_A");    //기본 애니메이션으로 변경(점프하면서 멈추는 것 방지용)   
         }
     }
 }
