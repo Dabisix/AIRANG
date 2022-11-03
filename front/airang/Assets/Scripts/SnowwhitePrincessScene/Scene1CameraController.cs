7fbe0ed39f1b5efedb10d7752002a48cc39e7406
Sukong <sukong0507@gmail.com>
2022-11-03 21:24:00 +0900

    feat : #29 난쟁이, 왕자, 노란옷백설공주, 마녀 모델 추가

--- front/airang/Assets/Scripts/SnowwhitePrincessScene/Scene1CameraController.cs
+++ front/airang/Assets/Scripts/SnowwhitePrincessScene/Scene1CameraController.cs
@@ -17,7 +17,7 @@ public class Scene1CameraController : MonoBehaviour
         Vector3 new_position = subCamera.transform.position;
 		Vector3 new_rotation = subCamera.transform.eulerAngles;
         iTween.MoveTo(this.gameObject, iTween.Hash("position", new_position, "easetype", iTween.EaseType.easeInOutQuart, "time", 7.0f));
-        iTween.RotateTo(this.gameObject, iTween.Hash("rotation", new_rotation, "easetype", iTween.EaseType.easeInOutQuart, "time", 7.0f));
+        iTween.RotateTo(this.gameObject, iTween.Hash("rotation", new_rotation, "easetype", iTween.EaseType.easeInOutQuart, "time", tjd7.0f));
     }
 
 	// Update is called once per frame
