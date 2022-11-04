823102235e30bfd88780d03df4ee15bf18f5ffc1
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-11-04 16:23:27 +0900

    feat : #24 add loading from GameManager list

--- front/airang/Assets/Scripts/MainScene/BookList/BookListLoader.cs
+++ front/airang/Assets/Scripts/MainScene/BookList/BookListLoader.cs
@@ -1,6 +1,8 @@
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,11 +14,62 @@ public class BookListLoader : MonoBehaviour
     [SerializeField]
     Transform contentContainer;
 
-    // Start is called before the first frame update
-    void Start()
+    public enum BookListType
     {
-        List<Book> books = GameManager.getInstance().Books;
+        FavorBooks,
+        RecentBooks,
+        Books,
+        PopularBooks,
+    };
 
+    [SerializeField]
+    public BookListType book_list_type;
+
+    private void Start()
+    {
+        // get Container not used
+        //contentContainer = this.gameObject.transform.Find("Viewport").Find("Content");
+    }
+
+
+    // load books with type of book list
+    public void loadBooks(bool mustRender)
+    {
+        if (mustRender || contentContainer.childCount == 0)
+        {
+            eraseAllBooks();
+            switch (book_list_type)
+            {
+                case BookListType.Books:
+                    // reder All books
+                    renderBooks(GameManager.getInstance().Books);
+                    break;
+                case BookListType.PopularBooks:
+                    // reder Popular books
+                    renderBooks(GameManager.getInstance().PopularBooks);
+                    break;
+                case BookListType.FavorBooks:
+                    // reder favorite books
+                    renderBooks(GameManager.getInstance().Favor_Books);
+                    break;
+                case BookListType.RecentBooks:
+                    // reder recently saw books
+                    renderBooks(GameManager.getInstance().RecentBooks);
+                    break;
+            }
+        }
+    }
+
+    void eraseAllBooks()
+    {
+        for(int i = 0; i < contentContainer.childCount; i++) {
+            Destroy(contentContainer.GetChild(i).gameObject);
+        }
+    }
+
+    // render at content in scrollView
+    void renderBooks(List<Book> books)
+    {
         for (int i = 0; i < books.Count; i++)
         {
             var item = Instantiate(bookItemPrefab);
@@ -27,6 +80,5 @@ public class BookListLoader : MonoBehaviour
             item.transform.localScale = new Vector3(1.15f, 1.37f, 1);
             item.transform.localPosition = new Vector3(item.transform.position.x, item.transform.position.y, 0);
         }
-
     }
 }
