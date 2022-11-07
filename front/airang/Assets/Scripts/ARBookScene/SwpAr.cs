6484c95fb945370d79b12f5e731101bdac35af49
kimsubni <81076792+kimsubni@users.noreply.github.com>
2022-11-07 13:12:05 +0900

    feat : #34 commit

---
+++ front/airang/Assets/Scripts/ARBookScene/SwpAr.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+[RequireComponent(typeof(ARAnchorManager))]
+[RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARPlaneManager))]
+public class SwpAr : MonoBehaviour
+{
+    public void loadContentPrefab()
+    {
+        RemoveAllAnchors();
+        m_AnchorPrefab = BookManager.getInstance().Content;
+    }
+
+    // 모든 Anchor를 삭제
+    public void RemoveAllAnchors()
+    {
+        // Anchor와 인스턴스화된 Object 모두 삭제
+        foreach (var anchor in m_AnchorPoints)
+            Destroy(anchor);
+        m_AnchorPoints.Clear();
+
+        Destroy(m_RendedObject);
+    }
+
+    void Awake()
+    {
+        // Awake 안에서 초기화 합시다!
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_AnchorManager = GetComponent<ARAnchorManager>();
+        m_PlaneManager = GetComponent<ARPlaneManager>();
+        m_AnchorPoints = new List<ARAnchor>();
+    }
+
+    private void Start()
+    {
+        loadContentPrefab();
+    }
+
+    void Update()
+    {
+        // Raycast 함수로 화면의 정중앙에서 레이져를 쏘고 해당 경로에 생성된 AR Plane이 있을 경우
+        // 여기 코드에서는 렌더링된 Anchor가 1개 미만일 경우라는 조건도 추가함
+        if (m_AnchorPoints.Count < 1 && m_RaycastManager.Raycast(new Vector2(Screen.width * 0.5f, Screen.height * 0.5f), s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+            /*
+                AR로 생성된 여러 객체들을 Trackable이라고 한다
+                ARRaycastManager의 Raycast는 자체적으로 Hit 정보에 어떤 Trackable이 맞았는지 알려준다
+            */
+            var hitTrackableId = s_Hits[0].trackableId;
+            var hitPlane = m_PlaneManager.GetPlane(hitTrackableId);
+
+
+
+            // Plane 정보를 가져오고 anchor를 생성, 그 Anchor위에 Prefab을 생성함
+            var anchor = m_AnchorManager.AttachAnchor(hitPlane, hitPose);
+            var created = Instantiate(m_AnchorPrefab, anchor.transform);
+
+            if (anchor == null)
+            {
+                Debug.Log("Error creating anchor.");
+            }
+            else
+            {
+                m_AnchorPoints.Add(anchor);
+                m_RendedObject = created;
+                TogglePlaneDetection();
+            }
+        }
+    }
+
+    // 프리팹을 만들고 나면 Plane Detection을 비활성화
+    void TogglePlaneDetection()
+    {
+        m_PlaneManager.enabled = !m_PlaneManager.enabled;
+
+        foreach (ARPlane plane in m_PlaneManager.trackables)
+        {
+            plane.gameObject.SetActive(m_PlaneManager.enabled);
+        }
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    List<ARAnchor> m_AnchorPoints;
+    public GameObject m_AnchorPrefab;
+    GameObject m_RendedObject;
+
+    ARRaycastManager m_RaycastManager;
+    ARAnchorManager m_AnchorManager;
+    ARPlaneManager m_PlaneManager;
+
+}
