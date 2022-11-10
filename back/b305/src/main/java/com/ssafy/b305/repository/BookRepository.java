63c9907b5e671817a218f825d0633a4fc360c09f
seoeun98 <vcv0174@naver.com>
2022-11-10 10:01:25 +0900

    feat : #42 bookRepo 테이블명 잘못되어있던 거 수정

--- back/b305/src/main/java/com/ssafy/b305/repository/BookRepository.java
+++ back/b305/src/main/java/com/ssafy/b305/repository/BookRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 public interface BookRepository extends JpaRepository<Book, Integer> {
     Optional<Book> findBybId(Long id);
 
-    @Query(value = "SELECT * FROM Book WHERE title LIKE %:keyword% AND a_flag=:aFlag", nativeQuery = true)
+
+    @Query(value = "SELECT * FROM book WHERE title LIKE %:keyword% AND a_flag=:aFlag", nativeQuery = true)
     List<Book> findBookList(@Param("keyword") String keyword, @Param("aFlag") int aFlag);
 }
