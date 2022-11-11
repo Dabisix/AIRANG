bdc528b178335bd6a0e65d5cec7ad18dfc6d2a35
이화연 <snw4002@hanmail.net>
2022-11-11 09:25:57 +0900

    feat : #40 녹음 다시 세팅

--- front/airang/Assets/Scripts/BookScene/ContentCreator.cs
+++ front/airang/Assets/Scripts/BookScene/ContentCreator.cs
@@ -1,12 +1,14 @@
+using Proyecto26;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ContentCreator : MonoBehaviour
 {
     public void loadContentPrefab()
     {
-        if(BookManager.getInstance() != null)
+        if (BookManager.getInstance() != null)
             m_Prefab = BookManager.getInstance().Content ?? null;
     }
 
@@ -14,8 +16,10 @@ public class ContentCreator : MonoBehaviour
     {
         if (m_RendedObject != null)
             Destroy(m_RendedObject);
-        else {
-            m_RendedObject = Instantiate(m_Prefab, new Vector3(0, 0), new Quaternion(0, 0, 0, 0));   
+        else
+        {
+            m_RendedObject = Instantiate(m_Prefab, new Vector3(0, 0), new Quaternion(0, 0, 0, 0));
+            getNarr(m_RendedObject);
             return;
         }
     }
@@ -24,8 +28,43 @@ public class ContentCreator : MonoBehaviour
     {
         loadContentPrefab();
         renderContent();
+        getNarr(m_RendedObject);
     }
 
     GameObject m_Prefab;
     GameObject m_RendedObject;
+
+    public void getNarr(GameObject gameObject)
+    {
+        Debug.Log(gameObject.name);
+        Debug.Log(BookManager.getInstance().ParentNarr ? "부모님" : "기본");
+        Debug.Log(!BookManager.getInstance().NeedRecord ? "녹음 필요없음" : "녹음 필요");
+        // 부모님 나레이션을 선택했고, 부모님 나레이션 녹음 파일이 있으면
+        if (BookManager.getInstance().ParentNarr && !BookManager.getInstance().NeedRecord)
+        {
+            Debug.Log("부모님 나레이션 들려줄거임");
+            AudioSource audio = gameObject.AddComponent<AudioSource>();
+            // TODO
+            var fileUrl = "file://" + Application.persistentDataPath + "/" + BookManager.getInstance().CurBook.BookId.ToString() + "/" + BookManager.getInstance().CurPage.ToString() + ".wav"; //모바일용
+            //var fileUrl = Application.persistentDataPath + "/" + BookManager.getInstance().CurBook.BookId.ToString() + "/" + BookManager.getInstance().CurPage.ToString() + ".wav"; //컴퓨터용
+            Debug.Log("파일 경로 : " + fileUrl);
+            var fileType = AudioType.WAV;
+
+            RestClient.Get(new RequestHelper
+            {
+                Uri = fileUrl,
+                DownloadHandler = new DownloadHandlerAudioClip(fileUrl, fileType)
+            }).Then(res => {
+                Debug.Log("성공???");
+                audio.clip = ((DownloadHandlerAudioClip)res.Request.downloadHandler).audioClip;
+                audio.mute = false;
+                audio.loop = false;
+                audio.Play();
+            }).Catch(err => {
+                Debug.Log("Error " + err.Message);
+            });
+        }
+
+    }
+
 }
---
+++ front/airang/Assets/Scripts/BookScene/RecordParentVoice.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class RecordParentVoice : MonoBehaviour
+{
+    public GameObject startRecord;
+    public GameObject stopRecord;
+
+    private string _microphoneID;
+    AudioClip recordClip;
+
+    float timer;
+    bool start;
+
+    BookManager bookManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bookManager = BookManager.getInstance();
+        Debug.Log("녹음 필요 여부 : " + bookManager.NeedRecord);
+        if (!bookManager.NeedRecord || !bookManager.ParentNarr)
+        {
+            startRecord.SetActive(false);
+            stopRecord.SetActive(false);
+        }
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+
+        _microphoneID = Microphone.devices[0];
+        recordClip = GetComponent<AudioClip>();
+    }
+
+    public void OnClickStart()
+    {
+        startRecord.SetActive(false);
+        stopRecord.SetActive(true);
+        Debug.Log("녹음이 시작됩니다.");
+        recordClip = Microphone.Start(_microphoneID, false, 1000, 44100);
+    }
+
+    public void OnClickEnd()
+    {
+        startRecord.SetActive(true);
+        stopRecord.SetActive(false);
+        Debug.Log("녹음이 끝난다?");
+        int lastTime = Microphone.GetPosition(null);
+
+        if (lastTime == 0)
+            return;
+        else
+        {
+            Microphone.End(_microphoneID);
+
+            float[] samples = new float[recordClip.samples];
+
+            recordClip.GetData(samples, 0);
+
+            float[] cutSamples = new float[lastTime];
+
+            Array.Copy(samples, cutSamples, cutSamples.Length - 1);
+
+            recordClip = AudioClip.Create("Notice", cutSamples.Length, 1, 44100, false);
+
+            recordClip.SetData(cutSamples, 0);
+        }
+
+        // 책 아이디가 폴더이름이고, 그 아래 각 페이지 번호를 이름으로 녹음 파일 저장
+        SavWav.Save(Application.persistentDataPath + "/" + bookManager.CurBook.BookId + "/" + bookManager.CurPage, recordClip);
+    }
+
+}
