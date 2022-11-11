899c0c6c18ce39183bfe43a21c43733c12f36c30
kimsubni <81076792+kimsubni@users.noreply.github.com>
2022-11-11 15:31:15 +0900

    feat : #45 detail arang main movement

--- front/airang/Assets/Scripts/MainScene/MainMainCamera.cs
+++ front/airang/Assets/Scripts/MainScene/MainMainCamera.cs
@@ -15,33 +15,37 @@ public class MainMainCamera : MonoBehaviour
     public GameObject[] paths;
     public void MainMoveCamera(int index)
     {
-        Vector3 new_position = subCameras[index].transform.position;
-        Vector3 new_rotation = subCameras[index].transform.eulerAngles;
-        //transform.position = new_position;
-        //transform.eulerAngles = new_rotation;
-        mainCharacterController.changePath = changePath;
-        iTween.MoveTo(this.gameObject, iTween.Hash("position", new_position, "easetype", iTween.EaseType.easeOutQuint, "time", 5.0f));
-        iTween.RotateTo(this.gameObject, iTween.Hash("rotation", new_rotation, "easetype", iTween.EaseType.easeOutQuint, "time", 5.0f));
-        if(index ==2)
+        if(gameObject.transform.position != subCameras[index].transform.position)
 		{
-            // if index is 2 ( selected camera is home camera), reverse path
+            Vector3 new_position = subCameras[index].transform.position;
+            Vector3 new_rotation = subCameras[index].transform.eulerAngles;
+            //transform.position = new_position;
+            //transform.eulerAngles = new_rotation;
+            mainCharacterController.changePath = changePath;
+            iTween.MoveTo(this.gameObject, iTween.Hash("position", new_position, "easetype", iTween.EaseType.easeOutQuint, "time", 5.0f));
+            iTween.RotateTo(this.gameObject, iTween.Hash("rotation", new_rotation, "easetype", iTween.EaseType.easeOutQuint, "time", 5.0f));
+            if (index == 2)
+            {
+                // if index is 2 ( selected camera is home camera), reverse path
 
-            CinemachinePath path = mainCharacterController.obj.m_Path.GetComponent<CinemachinePath>();
-            CinemachinePath new_path = mainCharacterController.changePath.GetComponent<CinemachinePath>();
-            new_path.gameObject.transform.position = path.gameObject.transform.position;
-            new_path.m_Waypoints.SetValue(path.m_Waypoints.GetValue(1), 0);
-            new_path.m_Waypoints.SetValue(path.m_Waypoints.GetValue(0), 1);
-            mainCharacterController.ChangePath(5); 
+                CinemachinePath path = mainCharacterController.obj.m_Path.GetComponent<CinemachinePath>();
+                CinemachinePath new_path = mainCharacterController.changePath.GetComponent<CinemachinePath>();
+                new_path.gameObject.transform.position = path.gameObject.transform.position;
+                new_path.m_Waypoints.SetValue(path.m_Waypoints.GetValue(1), 0);
+                new_path.m_Waypoints.SetValue(path.m_Waypoints.GetValue(0), 1);
+                mainCharacterController.ChangePath(5);
+            }
+            else
+            {
+                mainCharacterController.changePath = paths[index];
+                mainCharacterController.ChangePath(5);
+            }
+            if (index == 0)
+            {
+                bookListCanvas.SetActive(false);
+            }
         }
-		else
-		{
-            mainCharacterController.changePath = paths[index];
-            mainCharacterController.ChangePath(5);
-        }
-        if (index == 0)
-        {
-            bookListCanvas.SetActive(false);
-        }
+        
     }
     private void Start()
     {
