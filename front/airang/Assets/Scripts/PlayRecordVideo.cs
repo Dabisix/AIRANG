2ec4cedb34959243b07cb93f619b3a0e4834fbe2
이화연 <snw4002@hanmail.net>
2022-11-12 17:14:06 +0900

    feat : #53 녹화된 비디오 재생 틀 구현

---
+++ front/airang/Assets/Scripts/PlayRecordVideo.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Video; // 비디오 플레이서 사용
+
+public class PlayRecordVideo : MonoBehaviour
+{
+    public VideoPlayer video; //직접 할당
+    //VideoPlayer vp;
+
+    private void Start()
+    {
+        //string path1 = "비디오 불러올 주소";
+        //vp = GetComponent<VideoPlayer>();
+        //vp.url = System.IO.Path.Combine(path1, "이름");
+    }
+
+    public void startVideo()
+    {
+        video.Play();
+    }
+
+    public void pauseVideo()
+    {
+        video.Pause();
+    }
+
+    public void stopVideo()
+    {
+        video.Stop();
+    }
+
+    private const string subject = "원하는 제목";
+    private const string body = "https://youtu.be/PYNvQIJ2cuM";
+
+    public void Share()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+		using (AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent")) 
+		using (AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent")) {
+			intentObject.Call<AndroidJavaObject>("setAction", intentObject.GetStatic<string>("ACTION_SEND"));
+			intentObject.Call<AndroidJavaObject>("setType", "text/plain");
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_SUBJECT"), subject);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentObject.GetStatic<string>("EXTRA_TEXT"), body);
+			using (AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+			using (AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity")) 
+			using (AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "Share Via"))
+			currentActivity.Call("startActivity", jChooser);
+		}
+#endif
+    }
+}
