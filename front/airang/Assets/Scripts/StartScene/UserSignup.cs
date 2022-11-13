de7bcfdf04cf8d6a4fbe1cab8fd66ebce4f74a2b
이화연 <snw4002@hanmail.net>
2022-11-13 20:51:03 +0900

    fix : #48 로그인, 회원가입 터치 버그

--- front/airang/Assets/Scripts/StartScene/UserSignup.cs
+++ front/airang/Assets/Scripts/StartScene/UserSignup.cs
@@ -8,13 +8,43 @@ public class UserSignup : MonoBehaviour
 {
     public GameObject signupCanvas;
 
+    public bool IsPointerOverUIObject(Vector2 touchPos)
+    {
+        PointerEventData eventDataCurrentPosition
+            = new PointerEventData(EventSystem.current);
+
+        eventDataCurrentPosition.position = touchPos;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+
+
+        EventSystem.current
+        .RaycastAll(eventDataCurrentPosition, results);
+
+        return results.Count > 0;
+    }
+
     void OnMouseDown()
     {
-		if (!EventSystem.current.IsPointerOverGameObject())
-		{
-            // 클릭하면 회원가입 캔버스 활성화
-            openSignUpCanvas();
+        //터치
+        if (Input.touchCount > 0)
+        {
+            if (!IsPointerOverUIObject(Input.mousePosition))
+            {
+                //터치 처리
+                openSignUpCanvas();
+            }
         }
+
+        //마우스 
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsPointerOverUIObject(Input.mousePosition))
+            {
+                openSignUpCanvas();
+            }
+        }
+
     }
 
     public void openSignUpCanvas()
