1dbec0d3697bec2875431ad916bfe855fd425830
정수빈 <integerbin2@naver.com>
2022-11-14 15:49:14 +0900

    feat : #62 Click Audio 추가

--- front/airang/Assets/Scripts/BackgroundMusicScript.cs
+++ front/airang/Assets/Scripts/BackgroundMusicScript.cs
@@ -5,23 +5,58 @@ using UnityEngine.UI;
 
 public class BackgroundMusicScript : MonoBehaviour
 {
-    AudioSource audioSource;
+    AudioSource[] audioSource;  // 0: BGM, 1:click
     public Toggle toggle;
 
     private void Start()
     {
-        audioSource = GameManager.getInstance().GetComponent<AudioSource>();
+        audioSource = GameManager.getInstance().GetComponents<AudioSource>();
 
         // set sync toggle music
-        if (audioSource.isPlaying)
+        if (audioSource[0].isPlaying)
         {
             toggle.isOn = false;
         }
-            
     }
 
     public void toggleBGM()
     {
-        audioSource.mute = toggle.isOn;
+        foreach (var item in audioSource)
+        {
+            item.mute = toggle.isOn;
+        }
     }
+
+    //update : 클릭한 것이 button, toggle이면 소리 발생
+    public void Update()
+    {
+       //클릭한 것 찾기
+       if(TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            //터치 없으면 돌아가기
+            if (Input.touchCount == 0) return;
+            
+            //터치 있으면 
+            Touch touch = Input.GetTouch(0);
+            ray = Camera.main.ScreenPointToRay(touch.position);
+            Physics.Raycast(ray, out hitobj);
+            //해당 오브젝트에서 button, toggle 찾기
+
+            //있으면 소리
+        }
+    }
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    private Vector2 touchPosition;
+    private Ray ray;
+    private RaycastHit hitobj;
 }
